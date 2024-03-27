d6849c0438d27fe5f81684e6798197b549eb47eb
Zubairpv <subairpoptech@gmail.com>
2024-03-27 12:51:01 +0530

    updates on home screen

--- lib/view/screen/home_screen.dart
+++ lib/view/screen/home_screen.dart
@@ -4,7 +4,7 @@ import 'package:nexteons_test/controller/application/get_data_impl.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
-  final GetDataController getDataController = Get.find();
+  final GetDataController controller = Get.find();
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -12,56 +12,63 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: GetBuilder<GetDataController>(builder: (controller) {
-            
-              return DataTable(
-                  horizontalMargin: 10,
-                  dataRowMaxHeight: 120,
-                  columnSpacing: 20,
-                  headingTextStyle: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black),
-                  // Datatable widget that have the property columns and rows.
-                  columns: const [
-                    // Set the name of the column
-                    DataColumn(
-                      label: Text('Name'),
-                    ),
-                    DataColumn(
-                      label: Text('UID'),
-                    ),
-                    DataColumn(
-                      label: Text('Doc type'),
-                    ),
-                    DataColumn(
-                      label: Text('image'),
-                    ),
-                  ],
-                  rows: List.generate(
-                    controller.jwelleryDatas.length,
-                    (index) => DataRow(cells: [
-                      DataCell(SizedBox(
-                          width: size.width / 5,
-                          child: Text(controller.jwelleryDatas[index].name))),
-                      DataCell(SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: controller.obx(
+                onLoading: Center(
+                  child: CircularProgressIndicator(),
+                ),
+                onError: (error) {
+                  return Center(
+                    child: Text(error!),
+                  );
+                },
+                (state) => DataTable(
+                    horizontalMargin: 10,
+                    dataRowMaxHeight: 120,
+                    columnSpacing: 20,
+                    headingTextStyle: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                    // Datatable widget that have the property columns and rows.
+                    columns: const [
+                      // Set the name of the column
+                      DataColumn(
+                        label: Text('Name'),
+                      ),
+                      DataColumn(
+                        label: Text('UID'),
+                      ),
+                      DataColumn(
+                        label: Text('Doc type'),
+                      ),
+                      DataColumn(
+                        label: Text('image'),
+                      ),
+                    ],
+                    rows: List.generate(
+                      controller.jwelleryDatas.length,
+                      (index) => DataRow(cells: [
+                        DataCell(SizedBox(
+                            width: size.width / 5,
+                            child: Text(controller.jwelleryDatas[index].name))),
+                        DataCell(SizedBox(
+                            width: size.width / 10,
+                            child: Text(controller.jwelleryDatas[index].uid
+                                .toString()))),
+                        DataCell(SizedBox(
                           width: size.width / 10,
-                          child: Text(
-                              controller.jwelleryDatas[index].uid.toString()))),
-                      DataCell(SizedBox(
-                        width: size.width / 10,
-                        child: Text(
-                            controller.jwelleryDatas[index].docType.toString()),
-                      )),
-                      DataCell(SizedBox(
-                          width: size.width / 10,
-                          child: Image.network(
-                              controller.jwelleryDatas[index].url.toString()))),
-                    ]),
-                  ));
-            }),
-          )),
+                          child: Text(controller.jwelleryDatas[index].docType
+                              .toString()),
+                        )),
+                        DataCell(SizedBox(
+                            width: size.width / 10,
+                            child: Image.network(controller
+                                .jwelleryDatas[index].url
+                                .toString()))),
+                      ]),
+                    )),
+              ))),
     );
   }
 }
